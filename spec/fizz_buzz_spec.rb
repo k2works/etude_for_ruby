eac3ffde67ef66c2d6f3992b420e228d30250a20
k2works <kakimomokuri@gmail.com>
2017-04-17 18:13:34 +0900

    セットアップ
    クラスとテストケースを作成する。

---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+RSpec.describe FizzBuzz do
+  it "return Fizz if it is divisible by 3."
+  it "return Buzz if it is divisible by 5."
+  it "return FizzBuzz if it is divisible by 3 or 5."
+end
