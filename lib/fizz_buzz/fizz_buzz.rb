4b04c88fece1a29e1edc7e088161daf8713e79a2
k2works <kakimomokuri@gmail.com>
2017-04-17 18:27:28 +0900

    判定メソッド作成
    最低限の仕様を満たすメソッドを実装してテストを通るようにする。

--- lib/fizz_buzz/fizz_buzz.rb
+++ lib/fizz_buzz/fizz_buzz.rb
@@ -1,2 +1,8 @@
 class FizzBuzz
+  def self.execute(number)
+    return 'FizzBuzz' if (number % 3 == 0) && (number % 5 == 0)
+    return 'Fizz' if number % 3 == 0
+    return 'Buzz' if number % 5 == 0
+    return raise
+  end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,7 +1,15 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe FizzBuzz do
-  it "return Fizz if it is divisible by 3."
-  it "return Buzz if it is divisible by 5."
-  it "return FizzBuzz if it is divisible by 3 or 5."
+  describe '.execute' do
+    it 'return Fizz if it is divisible by 3.' do
+      expect(FizzBuzz.execute(3)).to eq('Fizz')
+    end
+    it 'return Buzz if it is divisible by 5.' do
+      expect(FizzBuzz.execute(5)).to eq('Buzz')
+    end
+    it 'return FizzBuzz if it is divisible by 3 or 5.' do
+      expect(FizzBuzz.execute(15)).to eq('FizzBuzz')
+    end
+  end
 end
