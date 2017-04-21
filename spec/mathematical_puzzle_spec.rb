4c75907769d72d273d1ebce2103861c12aa8dbbd
k2works <kakimomokuri@gmail.com>
2017-04-21 10:26:06 +0900

    テストケース作成 #11

--- spec/mathematical_puzzle_spec.rb
+++ spec/mathematical_puzzle_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe MathematicalPuzzle::MathematicalPuzzle do
-  it 'do something'
+  it 'becomes a palindrome when it is a decimal number'
+  it 'becomes a palindrome when it is a octal number'
+  it 'becomes a palindrome when it is a binary number'
+  it 'fulfill all of the above simultaneously.'
 end
