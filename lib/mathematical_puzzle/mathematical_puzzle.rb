d8f286a4cb1fd33805dae2bd2187b5d961f47a6f
k2works <kakimomokuri@gmail.com>
2017-04-21 12:22:13 +0900

    10進数で回文アルゴリズム実装 #11

--- lib/mathematical_puzzle/mathematical_puzzle.rb
+++ lib/mathematical_puzzle/mathematical_puzzle.rb
@@ -1,5 +1,23 @@
 module MathematicalPuzzle
   class MathematicalPuzzle
+    def palindrome(number,type)
+      case type
+        when 'DEC';number.to_s.reverse == number.to_s ? number.to_s : raise
+        when 'OCT';number.to_s(2).reverse == number.to_s(2) ? number.to_s(2) : raise
+        when 'BIN';number.to_s(8).reverse == number.to_s(8) ? number.to_s(8) : raise
+        else
+          raise
+      end
+    end
 
+    def return_minimum_palindrome
+      number = 11
+      while true
+        if number.to_s.reverse == number.to_s && number.to_s(2).reverse == number.to_s(2) && number.to_s(8).reverse == number.to_s(8)
+          return number.to_s
+        end
+        number += 1
+      end
+    end
   end
 end
--- spec/mathematical_puzzle_spec.rb
+++ spec/mathematical_puzzle_spec.rb
@@ -1,8 +1,29 @@
 require 'spec_helper'
 
 RSpec.describe MathematicalPuzzle::MathematicalPuzzle do
-  it 'becomes a palindrome when it is a decimal number'
-  it 'becomes a palindrome when it is a octal number'
-  it 'becomes a palindrome when it is a binary number'
-  it 'fulfill all of the above simultaneously.'
+  describe '#palindrome' do
+    it 'becomes a palindrome when it is a decimal number' do
+      o = MathematicalPuzzle::MathematicalPuzzle.new
+      expect(o.palindrome(11,'DEC')).to eq('11')
+    end
+    it 'becomes a palindrome when it is a octal number' do
+      o = MathematicalPuzzle::MathematicalPuzzle.new
+      expect(o.palindrome(9,'OCT')).to eq('1001')
+    end
+    it 'becomes a palindrome when it is a binary number' do
+      o = MathematicalPuzzle::MathematicalPuzzle.new
+      expect(o.palindrome(9,'BIN')).to eq('11')
+    end
+    it 'raise exception' do
+      o = MathematicalPuzzle::MathematicalPuzzle.new
+      expect{o.palindrome(12,'NON')}.to raise_exception
+    end
+  end
+
+  describe '#return_minimum_palindrome' do
+    it 'fulfill all of the above simultaneously.' do
+      o = MathematicalPuzzle::MathematicalPuzzle.new
+      expect(o.return_minimum_palindrome).to eq('585')
+    end
+  end
 end
