634b6fde0a382c7f8e74c420406455f1c63c2993
k2works <kakimomokuri@gmail.com>
2017-04-22 11:00:06 +0900

    リファクタリング #11
    テストケースの内容を更新する。

--- spec/mathematical_puzzle_spec.rb
+++ spec/mathematical_puzzle_spec.rb
@@ -2,31 +2,32 @@ require 'spec_helper'
 
 RSpec.describe MathematicalPuzzle::Palindrome do
   describe '#return_palindrome' do
-    it 'becomes a palindrome when it is a decimal number' do
-      t = MathematicalPuzzle::Palindrome::DEC
-      o = MathematicalPuzzle::Palindrome.new(11,t)
-      expect(o.return_palindrome).to eq('11')
+    it 'return a decimal palindrome when it is a decimal number' do
+      i = create_palindrome_instance(11, MathematicalPuzzle::Palindrome::DEC)
+      expect(i.return_palindrome).to eq('11')
     end
-    it 'becomes a palindrome when it is a octal number' do
-      t = MathematicalPuzzle::Palindrome::OCT
-      o = MathematicalPuzzle::Palindrome.new(9,t)
-      expect(o.return_palindrome).to eq('1001')
+    it 'return a octal palindrome when it is a octal number' do
+      i = create_palindrome_instance(9, MathematicalPuzzle::Palindrome::OCT)
+      expect(i.return_palindrome).to eq('1001')
     end
-    it 'becomes a palindrome when it is a binary number' do
-      t = MathematicalPuzzle::Palindrome::BIN
-      o = MathematicalPuzzle::Palindrome.new(9,t)
-      expect(o.return_palindrome).to eq('11')
+    it 'return a binary palindrome when it is a binary number' do
+      i = create_palindrome_instance(9, MathematicalPuzzle::Palindrome::BIN)
+      expect(i.return_palindrome).to eq('11')
     end
-    it 'raise exception' do
-      o = MathematicalPuzzle::Palindrome.new(12,'NON')
-      expect{o.return_palindrome}.to raise_exception
+    it 'raise exception when it is not palindrome' do
+      i = create_palindrome_instance(12, 'NON')
+      expect{i.return_palindrome}.to raise_exception
     end
   end
 
-  describe '#return_minimum_palindrome' do
-    it 'fulfill all of the above simultaneously.' do
-      o = MathematicalPuzzle::Palindrome.return_minimum_palindrome(11)
-      expect(o).to eq('585')
+  describe '.return_minimum_palindrome' do
+    it 'fulfill all of decimal octal binary palindrome simultaneously.' do
+      i = MathematicalPuzzle::Palindrome.return_minimum_palindrome(11)
+      expect(i).to eq('585')
     end
+  end
+
+  def create_palindrome_instance(number, type)
+    MathematicalPuzzle::Palindrome.new(number, type)
   end
 end
