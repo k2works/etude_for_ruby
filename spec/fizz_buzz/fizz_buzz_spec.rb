b99ce992d2196df6956615071e4c84a2e8378c4f
k2works <kakimomokuri@gmail.com>
2017-12-05 19:38:03 +0900

    イテレーション１

--- spec/fizz_buzz/fizz_buzz_spec.rb
+++ spec/fizz_buzz/fizz_buzz_spec.rb
@@ -3,25 +3,22 @@
 require 'spec_helper'
 
 RSpec.describe FizzBuzz do
-  # Divide
   describe '#divide' do
-    ## 3/3=Fizz
     it 'return fizz' do
       result = FizzBuzz.divide(3)
       expect('fizz').to eq result
     end
-    ## 5/5=Bizz
+
     it 'return buzz' do
       result = FizzBuzz.divide(5)
       expect('buzz').to eq result
     end
-    ## 15/3=FizzBuzz
-    ## 15/5=FizzBuzz
+
     it 'return fizzbuzz' do
       result = FizzBuzz.divide(15)
       expect('fizzbuzz').to eq result
 
-      result = FizzBuzz.divide(40)
+      result = FizzBuzz.divide(45)
       expect('fizzbuzz').to eq result
     end
   end
