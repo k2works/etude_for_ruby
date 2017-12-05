18a4eb25394101cf814d213cc7c1d731c07d9e26
k2works <kakimomokuri@gmail.com>
2017-12-05 18:08:36 +0900

    イテレーション１

--- lib/fizz_buzz/fizz_buzz.rb
+++ lib/fizz_buzz/fizz_buzz.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# FizzBuzz operation
 class FizzBuzz
+  def self.divide(_dividend)
+    'fizz'
+  end
 end
--- spec/fizz_buzz/fizz_buzz_spec.rb
+++ spec/fizz_buzz/fizz_buzz_spec.rb
@@ -4,10 +4,16 @@ require 'spec_helper'
 
 RSpec.describe FizzBuzz do
   # Divide
-  ## 3/3=Fizz
-  ## 5/5=Bizz
-  ## 15/3=FizzBuzz
-  ## 15/5=FizzBuzz
+  describe '#divide' do
+    ## 3/3=Fizz
+    it 'return fizz' do
+      result = FizzBuzz.divide(3)
+      expect('fizz').to eq result
+    end
+    ## 5/5=Bizz
+    ## 15/3=FizzBuzz
+    ## 15/5=FizzBuzz
+  end
   # Execute
   ## 100回繰り返し実行する
 end
